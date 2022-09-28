e8f2c9923d72544aa196006ab204866501c82c6b
Ashraful Islam <ashrafunit7@gmail.com>
2022-09-28 22:19:56 +0600

    generating original link

--- script.js
+++ script.js
@@ -6,8 +6,13 @@ const selectElement = (selector) => {
 
 let form = document.getElementById("form")
 let input = document.getElementById("urlInput")
+
+const formTwo = document.getElementById("formTwo");
+const inputTwo = document.getElementById("urlTwo");
+
 const result = selectElement(".result");
 const shortLink = document.getElementById("shortLink")
+const longLink = document.getElementById("longLink")
 // var urlBtn = document.getElementById("urlBtn")
 
 form.addEventListener("submit", (e) => {
@@ -30,3 +35,31 @@ form.addEventListener("submit", (e) => {
       console.log(err);
     }
   }
+
+
+
+
+
+  formTwo.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const url = inputTwo.value;
+    const code = url.split("/")[1]
+    expandUrl(code);
+  });
+
+  async function expandUrl(code) {
+    try {
+      const res = await fetch(`https://api.shrtco.de/v2/info?code=${code}`);
+      const data = await res.json();
+      console.log(data);
+      longLink.innerText = data.result.url;
+      const copyBtnTwo = document.getElementById("copyBtnTwo");
+      copyBtnTwo.addEventListener("click", () => {
+        navigator.clipboard.writeText(copyBtnTwo.previousElementSibling.textContent);
+      });
+      inputTwo.value = "";
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
